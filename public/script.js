727c57e47426980b75688323f1f01d04a6044322
eurzua7020 <77946291+eurzua7020@users.noreply.github.com>
2021-03-02 23:25:20 -0500

    Merge branch 'main' of https://github.com/Ananth3Rao/Assignment-Base

--- public/script.js
+++ public/script.js
@@ -20,10 +20,12 @@ function displayMatches() {
         return `
         <div class="block">
         <ul>
-        <li>
+        <li class="results">
             <span class="name"> ${resturant.name} </span><br />
-            <span class ="category"> ${resturant.category} </span> <br />
-            <span class ="address"> ${resturant.address_line_1} </span> <br />
+            <span class="category"> ${resturant.category} </span> <br />
+            <span class="address"> ${resturant.address_line_1} </span> <br />
+            <span class="city"> ${resturant.city}</span> <br />
+            <span class="zip">${resturant.zip}</span> 
         </li>
         </ul>
         </div>
